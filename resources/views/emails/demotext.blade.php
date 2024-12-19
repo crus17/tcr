6f0d92999b262828aecb3309bb6c4e33e79cc6c1
crus17 <christopher.crus17@gmail.com>
2024-12-19 03:18:07 +0100

    Updated email template

--- resources/views/emails/demotext.blade.php
+++ resources/views/emails/demotext.blade.php
@@ -1,18 +1,17 @@
 @component('mail::message')
 
 # Welcome to {{ $demo->sender }}!
-Dear {{ $demo->receiver_name }} Thank you for choosing {{ $demo->sender }} as your Crypto Investment Platform. Your account has been successfully created. Make sure to keep your login details safe for future references. For safety and security reasons, never share your login details or password with anyone. 
+Dear {{ $demo->receiver_name }},<br>
+Thank you for choosing {{ $demo->sender }} as your Crypto Investment Platform. Your account has been successfully created. Make deposit now into your investment account to start earning daily profits.
 
-A large list of tradeable Assets, Indices, Stocks and Commodities CFD and Crypto Investment using the MTA trading platform and Bitcoin mining. 
+Make sure to keep your login details safe for future references. For safety and security reasons, never share your login details or password with anyone. 
+
+A large list of tradeable Assets, Indices, NFT art, gold, Stocks and Commodities CFD with Crypto Investment using the MTA trading platform and Bitcoin mining.
 
 Please contact us at {{ $demo->contact_email }} with your email or your updated contact number or visit our contact us page. <br><br>
 
 Kind regards,<br>
 {{ $demo->sender }}.<br><br>
-
---------------------------------
-Email: {{ $demo->receiver_email }}<br>
-Password: {{ $demo->receiver_password }}
 @endcomponent
 
 
